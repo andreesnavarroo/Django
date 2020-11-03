cc79d6ac4b37cf9f9ff1274950ba9285dc0ef0a9
andreesnavarroo <andreesnavarroo@gmail.com>
2020-11-02 22:43:42 -0500

    vista basada en clase LISTVIEW category
    vista basada en clase listado de categoria

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from core.erp.views.category.views import category_list
+from core.erp.views.category.views import category_list, CategoryListView
 
 app_name = 'erp'
 
 urlpatterns = [
-    path('category/list', category_list, name='category_list'),
+    path('category/list', CategoryListView.as_view(), name='category_list'),
     
 ]
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from core.erp.models import Category
+from core.erp.models import Category, Product
+
+from django.views.generic import ListView
+
 def category_list(request):
     data = {
         'title': 'Listado de Categorias',
         'categories': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
+
+class  CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    # con este metodo modificamos la consulta 
+    def get_queryset(self):
+        return Category.objects.filter(name__startswith='b')
+
+
+    # devuelve un diccionario y podemos pasarle mas variables a nuestro contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorias'
+        return context        
