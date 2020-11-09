67c41c7c398181934f73a2611230e54681bf4d79
andreesnavarroo <andreesnavarroo@gmail.com>
2020-11-09 16:38:30 -0500

    CreateView category | Personalizando nuestro formulario

--- app/config/settings.py
+++ app/config/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Libs
+    'widget_tweaks',
     # Apps
-    'core.erp'
+    'core.erp',
+    
 ]
 
 MIDDLEWARE = [
