8cb46e212787a7279cc62c68aa3fc39725c25485
andreesnavarroo <andreesnavarroo@gmail.com>
2020-11-09 22:06:02 -0500

    UpdateView | Editando un registro

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from core.erp.views.category.views import category_list, CategoryListView, CategoryCreateView
+from core.erp.views.category.views import category_list, CategoryListView, CategoryCreateView, CategoryUpdateView
 
 app_name = 'erp'
 
 urlpatterns = [
     path('category/list/', CategoryListView.as_view(), name='category_list'),
     path('category/add/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/edit/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
     
 ]
