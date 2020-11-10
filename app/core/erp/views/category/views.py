9df146a795e1bf72018485be88a20d4c6099d2c8
andreesnavarroo <andreesnavarroo@gmail.com>
2020-11-09 22:47:37 -0500

    delete con ajax

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -91,6 +91,7 @@ class CategoryUpdateView(UpdateView):
     def dispatch(self, request, *args, **kwargs):  
         self.object = self.get_object()
         return super().dispatch(request, *args, **kwargs)
+
   
     def post(self, request, *args, **kwargs):
         data = {}
@@ -122,7 +123,19 @@ class CategoryDeleteView(DeleteView):
     template_name  = 'category/delete.html'
     success_url  = reverse_lazy('erp:category_list')
 
-            
+    def dispatch(self, request, *args, **kwargs):  
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+        
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            self.object.delete()                
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)    
+        
+                    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Eliminar una Categoria'
