0aa8ad24d7e9e44098ff9fb0cad6e1bf1529e6f9
andreesnavarroo <andreesnavarroo@gmail.com>
2020-11-09 22:05:44 -0500

    UpdateView | Editando un registro

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from core.erp.models import Category, Product
 from django.http import JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from core.erp.forms import CategoryForm
@@ -80,4 +80,36 @@ class CategoryCreateView(CreateView):
         context['entity'] = 'Categorias'
         context['list_url'] = reverse_lazy('erp:category_list')
         context['action'] = 'add'
+        return context
+
+class CategoryUpdateView(UpdateView):        
+    model = Category
+    form_class = CategoryForm
+    template_name  = 'category/create.html'
+    success_url  = reverse_lazy('erp:category_list')
+    
+    def dispatch(self, request, *args, **kwargs):  
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+  
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'edit':
+                # form = CategoryForm(request.POST)
+                form = self.get_form()
+                data = form.save()                    
+            else:
+                data['error'] = 'No ha ingresado a ninguna opcion'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Editar una Categoria'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
+        context['action'] = 'edit'
         return context
